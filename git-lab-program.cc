63a51b762db5bbb188d9ea96fe78ee11f3c28f5e
James Fernandez <jf794018@ohio.edu>
2020-01-21 11:24:14 -0500

    add git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: James Fernandeez
+ *        Date: January 21st, 2020
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    "Hello Git!!" << end1;
+    return 0;
+}// main
